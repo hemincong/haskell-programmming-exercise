28bebfcde8ee39b718636454d081fe6e5813b9f2
mincong.he <hemc@edaotech.com>
2017-02-26 23:32:47 +0800

    MyEitherLib add more function

--- chapter12/MyEitherLib.hs
+++ chapter12/MyEitherLib.hs
@@ -12,11 +12,24 @@ lefts2 = foldr (\x b -> case x of
                       (Left1 a1) -> a1:b
                       (Right1 b1) -> b) []
                       
+rights' :: [Either1 a b] -> [b]
+rights' [] = []
+rights' ((Left1 _):xs) = rights' xs
+rights' ((Right1 b1):xs) = b1:rights' xs
+                      
 partitionEithers' :: [Either1 a b] -> ([a], [b])
 partitionEithers' [] = ([], [])
-partitionEithers' ((Left1 a):xs) = (a:(fst $ partitionEithers' xs), snd (partitionEithers' xs))
-partitionEithers' ((Right1 b):xs) = ((fst $ partitionEithers' xs), (b:(snd $ partitionEithers' xs)))
+partitionEithers' xs = (lefts' xs, rights' xs)
+
+eitherMaybe' :: (b->c) -> Either1 a b -> Maybe c
+eitherMaybe' _ (Left1 _) = Nothing
+eitherMaybe' f (Right1 b) = Just (f b)
+
+either' :: (a -> c) -> (b -> c) -> Either1 a b -> c
+either' fl _ (Left1 a) = fl a
+either' _ fr (Right1 b) = fr b
                       
 
-
+eitherMaybe'' :: (b->c)->Either1 a b -> Maybe c
+eitherMaybe'' f = either' (\_ -> Nothing) (\b -> Just (f b))
                       
