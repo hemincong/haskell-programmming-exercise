ad9b0a68999e8caee7028b2911843a1175b69b26
mincong.he <hemc@edaotech.com>
2017-06-20 09:15:26 +0800

    + Combinations

---
+++ chapter17/Combinations.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Control.Applicative (lifeA3)
+
+stops :: String
+stops = "pbtdkg"
+
+vowels :: String
+vowels = "aeiou"
+
+combos :: [a] -> [b] -> [c] -> [(a, b, c)]
+combos = liftA3 (,,)
